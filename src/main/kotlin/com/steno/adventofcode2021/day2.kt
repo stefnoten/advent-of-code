44a39b04f7a76b1c4af1d1d5dcee74760dee2614
Stef Noten <stef.noten@gmail.com>
2021-12-02 20:53:02 +0100

    Extract move operations

--- src/main/kotlin/com/steno/adventofcode2021/day2.kt
+++ src/main/kotlin/com/steno/adventofcode2021/day2.kt
@@ -1,38 +1,44 @@
 package com.steno.adventofcode2021
 
-enum class Direction {
-    forward,
-    down,
-    up
+import com.steno.adventofcode2021.Direction.*
+import java.util.*
+
+fun main() {
+    Assignment("day2", Move::parse)
+        .eval { it.fold(Location(), ::move).value }
+        .eval { it.fold(Location(), ::moveAimed).value }
 }
 
 data class Move(val direction: Direction, val steps: Int) {
     override fun toString() = "$direction $steps"
+
+    companion object {
+        fun parse(value: String) = value.split(' ').let { (direction, steps) -> Move(Direction.parse(direction), steps.toInt()) }
+    }
+}
+
+enum class Direction {
+    FORWARD, DOWN, UP;
+
+    companion object {
+        fun parse(value: String) = valueOf(value.uppercase(Locale.getDefault()))
+    }
 }
 
 data class Location(val horizontal: Int = 0, val depth: Int = 0, val aim: Int = 0) {
-    fun move(move: Move) = when (move.direction) {
-        Direction.forward -> copy(horizontal = horizontal + move.steps)
-        Direction.down -> copy(depth = depth + move.steps)
-        Direction.up -> copy(depth = depth - move.steps)
-    }
-
-    fun moveAimed(move: Move) = when (move.direction) {
-        Direction.forward -> copy(horizontal = horizontal + move.steps, depth = depth + aim * move.steps)
-        Direction.down -> copy(aim = aim + move.steps)
-        Direction.up -> copy(aim = aim - move.steps)
-    }
-
-    val value: Int
-        get() = horizontal * depth
+    val value get() = horizontal * depth
 
     override fun toString() = "($horizontal, ${-depth}) ∠ ${-aim}"
 }
 
-private fun main() {
-    Assignment("day2", ::toMove)
-        .eval { it.fold(Location(), Location::move).value }
-        .eval { it.fold(Location(), Location::moveAimed).value }
+fun move(location: Location, move: Move) = when (move.direction) {
+    FORWARD -> location.copy(horizontal = location.horizontal + move.steps)
+    DOWN -> location.copy(depth = location.depth + move.steps)
+    UP -> location.copy(depth = location.depth - move.steps)
 }
 
-fun toMove(value: String) = value.split(' ').let { (direction, steps) -> Move(Direction.valueOf(direction), steps.toInt()) }
+fun moveAimed(location: Location, move: Move) = when (move.direction) {
+    FORWARD -> location.copy(horizontal = location.horizontal + move.steps, depth = location.depth + location.aim * move.steps)
+    DOWN -> location.copy(aim = location.aim + move.steps)
+    UP -> location.copy(aim = location.aim - move.steps)
+}
