1b6f292a70878204b7a22262720b6b542b7d07b1
Stef Noten <stef.noten@gmail.com>
2021-12-14 08:39:22 +0100

    2021 day14 - performance for step2

--- src/main/kotlin/com/steno/adventofcode/y2021/day14/day14.kt
+++ src/main/kotlin/com/steno/adventofcode/y2021/day14/day14.kt
@@ -4,19 +4,29 @@ import com.steno.adventofcode.util.inOrder
 import com.steno.adventofcode.util.parse
 import com.steno.assignment
 
-data class Polymer(val pairs: Sequence<Pair<Char, Char>>, val rules: Map<Pair<Char, Char>, Char>) {
+data class Polymer(val pairs: Map<Pair<Char, Char>, Long>, val rules: Map<Pair<Char, Char>, Char>) {
     val occurrences
-        get() = pairs.inOrder({ it.first().let { f -> sequenceOf(f.first to f.first, f) } }, { it }).let { (a, b) -> a + b }
-            .groupingBy { it.second }
-            .eachCount()
+        get() = pairs.asSequence().inOrder(
+            { it.first().let { (pair, count) -> sequenceOf((pair.first to pair.first) to count, pair to count) } },
+            { it.map { (pair, count) -> pair to count } }
+        )
+            .let { (a, b) -> a + b }
+            .groupingBy { (pair, _) -> pair.second }
+            .fold(0L) { acc, (_, count) -> acc + count }
+
     val score = occurrences.let { occ -> occ.maxOf { it.value } - occ.minOf { it.value } }
 
     fun steps(count: Int) = (1..count).fold(this) { acc, _ -> acc.step() }
-    fun step() = pairs.flatMap { pair ->
-        rules[pair]
-            ?.let { sequenceOf(pair.first to it, it to pair.second) }
-            ?: sequenceOf(pair)
-    }.let { Polymer(it, rules) }
+    fun step() = pairs.asSequence()
+        .flatMap { (pair, count) ->
+            when (val inserted = rules[pair]) {
+                null -> sequenceOf(pair to count)
+                else -> sequenceOf((pair.first to inserted) to count, (inserted to pair.second) to count)
+            }
+        }
+        .groupingBy { (pair, _) -> pair }
+        .fold(0L) { accumulator, (_, count) -> accumulator + count }
+        .let { Polymer(it, rules) }
 }
 
 private fun main() {
@@ -30,7 +40,13 @@ private fun main() {
 }
 
 fun parse(lines: Sequence<String>) = lines.inOrder(
-    { it.first().asSequence().zipWithNext() },
+    {
+        it.first().asSequence()
+            .zipWithNext()
+            .groupingBy { pair -> pair }
+            .eachCount()
+            .mapValues { (_, v) -> v.toLong() }
+    },
     { it.dropWhile(String::isEmpty).map(::parseRule) }
 ).let { (initial, rules) -> Polymer(initial, rules.toMap()) }
 
