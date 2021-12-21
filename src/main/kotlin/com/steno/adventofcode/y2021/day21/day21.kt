8c4641e78e297abbea940cb3774328ebbae255d5
Stef Noten <stef.noten@gmail.com>
2021-12-22 00:45:00 +0100

    2021 day21 part 2

--- src/main/kotlin/com/steno/adventofcode/y2021/day21/day21.kt
+++ src/main/kotlin/com/steno/adventofcode/y2021/day21/day21.kt
@@ -3,7 +3,7 @@ package com.steno.adventofcode.y2021.day21
 import com.steno.adventofcode.util.parse
 import com.steno.assignment
 
-data class Player(val number: Int, val position: Position, val score: Int = 0) {
+data class Player(val id: Int, val position: Position, val score: Int = 0) {
     fun advance(steps: Int) = (position + steps).let {
         copy(position = it, score = score + it.value)
     }
@@ -39,6 +39,29 @@ private fun main() {
                     }
                 }
         }
+        .eval { (player1, player2) -> wins(player1, player2, 21).byPlayer.maxOf { it.value } }
+}
+
+val STEPS_FREQUENCIES = (1..3).flatMap { x -> (1..3).flatMap { y -> (1..3).map { z -> x + y + z } } }
+    .groupingBy { it }.eachCount()
+
+data class Wins(val byPlayer: Map<Int, Long>) {
+    constructor(player: Player, wins: Long): this(mapOf(player.id to wins))
+
+    operator fun times(factor: Int) = Wins(byPlayer.mapValues { (_, wins) -> wins * factor })
+    operator fun plus(other: Wins) = Wins(
+        (byPlayer.entries.asSequence() + other.byPlayer.entries.asSequence())
+            .groupingBy { it.key }
+            .fold(0L) { acc, (_, wins) -> acc + wins }
+    )
+}
+
+fun wins(activePlayer: Player, otherPlayer: Player, targetScore: Int): Wins = when {
+    activePlayer.score >= targetScore -> Wins(activePlayer, 1)
+    otherPlayer.score >= targetScore -> Wins(otherPlayer, 1)
+    else -> STEPS_FREQUENCIES.entries
+        .map { (steps, frequency) -> wins(otherPlayer, activePlayer.advance(steps), targetScore) * frequency }
+        .reduce(Wins::plus)
 }
 
 val PATTERN = Regex("Player (\\d+) starting position: (\\d+)")
