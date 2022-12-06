382e97b9476205dd8d0e87ca4e0e3e497b3f8199
Stef Noten <stef.noten@gmail.com>
2022-12-06 07:37:00 +0100

    2022 Day 6

---
+++ src/test/kotlin/com/steno/adventofcode/y2022/Day6.kt
@@ -0,0 +1,13 @@
+package com.steno.adventofcode.y2022
+
+import com.steno.adventofcode.spec.AdventOfCodeSpec
+
+class Day6 : AdventOfCodeSpec({
+    parseAllLines { it.first() }
+        .eval { line -> line.indexOfMarker(4) + 4 }
+        .eval { line -> line.indexOfMarker(14) + 14 }
+})
+
+private fun String.indexOfMarker(size: Int) = asSequence()
+    .windowed(size)
+    .indexOfFirst { it.toSet().size == size }
