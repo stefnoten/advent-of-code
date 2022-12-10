cec45913321522714cabd0a465b9178aa6554981
Stef Noten <stef.noten@gmail.com>
2022-12-10 19:03:34 +0100

    Alternative

--- src/test/kotlin/com/steno/adventofcode/y2022/Day10.kt
+++ src/test/kotlin/com/steno/adventofcode/y2022/Day10.kt
@@ -7,15 +7,24 @@ import com.steno.adventofcode.y2022.Day10.AddX
 import com.steno.adventofcode.y2022.Day10.Noop
 
 class Day10 : AdventOfCodeSpec({ challenge ->
-    challenge.mapEach { it.toInstruction() }
-        .map { it + generateSequence { Noop } }
-        .map { it.iterator() }
+    challenge
+        .mapEach { it.toInstruction() }
         .map { instructions ->
-            generateSequence(State()) {
-                it.tick { instructions.next() }
-            }.drop(1)
+            instructions
+                .flatMap {
+                    when (it) {
+                        is AddX -> sequenceOf(Noop, it)
+                        else -> sequenceOf(it)
+                    }
+                }
+                .runningFold(State()) { state, instruction ->
+                    when (instruction) {
+                        is AddX -> state.copy(cycle = state.cycle + 1, x = state.x + instruction.value)
+                        is Noop -> state.copy(cycle = state.cycle + 1)
+                    }
+                }
         }
-        .eval(-720, 13140, 12640) { states ->
+        .eval(0, 13140, 12640) { states ->
             states
                 .filter { (it.cycle - 20) % 40 == 0 }
                 .takeUntil { it.cycle >= 220 }
@@ -31,27 +40,15 @@ class Day10 : AdventOfCodeSpec({ challenge ->
         }
 }) {
     data class State(
-        val cycle: Int = 0,
+        val cycle: Int = 1,
         val x: Int = 1,
-        val remainingCycles: Int = 0,
-        val action: () -> Int = { x },
     ) {
         val signalStrength = cycle * x
         private val spritePixels = x - 1..x + 1
         private val drawingPixel = cycle % 40 - 1
         val lit = drawingPixel in spritePixels
 
-        fun tick(next: () -> Instruction) = when (remainingCycles) {
-            0 -> copy(x = action()).handle(next())
-            else -> copy(cycle = cycle + 1, remainingCycles = remainingCycles - 1)
-        }
-
-        private fun handle(instruction: Instruction) = when (instruction) {
-            Noop -> copy(cycle = cycle + 1, remainingCycles = 0, action = { x })
-            is AddX -> copy(cycle = cycle + 1, remainingCycles = 1, action = { x + instruction.value })
-        }
-
-        override fun toString() = "[$cycle] X=$x (remaining: $remainingCycles)"
+        override fun toString() = "[$cycle] X=$x"
     }
 
     sealed interface Instruction {
